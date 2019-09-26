c8f8219f4b677217a51ce2f876b294431d8af23e
Urfan Guliyev <urfan196@yahoo.com>
2019-09-26 02:19:04 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,12 +1,12 @@
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
--- ("Mr. Chocolate", 2, "female", "brown", "mad", true),
--- ("Rowdy", 10, "male", "brown", "mad", true),
--- ("Tabitha", 3, "male", "black", "mad", false),
--- ("Sergeant Brown", 3, "male", "brown", "mad", true),
--- ("Melissa", 5, "female", "black", "mad", false),
--- ("Grinch", 2, "male", "brown", "mad", true),
--- ("Wendy", 1, "female", "black", "happy", false)
--- (null, 3, "male", "brown", "mad", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 2, "female", "brown", "mad", true),
+("Rowdy", 10, "male", "brown", "mad", true),
+("Tabitha", 3, "male", "black", "mad", false),
+("Sergeant Brown", 3, "male", "brown", "mad", true),
+("Melissa", 5, "female", "black", "mad", false),
+("Grinch", 2, "male", "brown", "mad", true),
+("Wendy", 1, "female", "black", "happy", false)
+(null, 3, "male", "brown", "mad", true);
 
 
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "female", "brown", "mad", true);
@@ -17,28 +17,3 @@
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "male", "brown", "mad", true);
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "female", "black", "happy", false);
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 3, "male", "brown", "mad", true);
-
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Mr. Chocolate", 20, "Male", "Chocolate", "Sweet", true);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Rowdy", 6, "Female", "Red", "Angry", true);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Tabitha", 28, "Female", "Purple", "Confounded", false);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Sergeant Brown", 89, "Male", "Grey", "Sour", false);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Melissa", 19, "Female", "Pink", "Kind", true);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Grinch", 22, "Male", "Green", "Mischievous", true);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Wendy", 13, "Female", "Turquoise", "Wicked", true);
-
-INSERT INTO bears (age, gender, color, temperament, alive)
-VALUES (30, "Female", "Spotted", "Absent", true);
